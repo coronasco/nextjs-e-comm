f17cedcbc6c583dde2d4055419c1621bde4291e0
coronasco <daniel.zaharia.corona@gmail.com>
2021-08-14 17:53:26 +0200

    try carousel

--- components/featured.js
+++ components/featured.js
@@ -1,18 +1,63 @@
-import {items} from '../data/items'
-import Item from './item'
+import { useEffect, useCallback } from "react";
+import { items } from "../data/items";
+import Item from "./item";
+import { useEmblaCarousel } from "embla-carousel/react";
+
 function Featured(props) {
-    return (
-        <div>
-            <div>
-                <button>Featured</button>
-                <button>On Sale</button>
-                <button>Top Rated</button>
-            </div>
+  const [emblaRef, embla] = useEmblaCarousel({
+    slidesToScroll: 2,
+    skipSnaps: false
+  });
+
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+  }, [embla]);
+
+  useEffect(() => {
+    if (!embla) return;
+    embla.on("select", onSelect);
+    onSelect();
+  }, [embla, onSelect]);
+
+  const scrollPrev = useCallback(() => {
+    if (embla) embla.scrollPrev();
+  }, [embla]);
+
+  const scrollNext = useCallback(() => {
+    if (embla) embla.scrollNext();
+  }, [embla]);
+
+  return (
+    <div className="mt-10">
+      <div>
+        <button>Featured</button>
+        <button>On Sale</button>
+        <button>Top Rated</button>
+      </div>
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
             {items.map(x => {
-                return <Item key={x.key} title={x.name} price={x.price} image={x.img} />
+              return (
+                <Item
+                  key={x.key}
+                  title={x.name}
+                  price={x.price}
+                  image={x.img}
+                />
+              );
             })}
+          </div>
         </div>
-    )
+        <button class="embla__prev" onClick={scrollPrev}>
+          Prev
+        </button>
+        <button class="embla__next" onClick={scrollNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Featured
+export default Featured;
--- components/item.js
+++ components/item.js
@@ -1,13 +1,17 @@
-function Item({title, price, image}) {
-    return (
-        <div className='w-full sm:w-[200px] h-[200px] overflow-hidden relative p-4 text-white'>
-            <div className='relative z-50'>
-                <div>{title}</div>
-                <div>{price}</div>
-            </div>
-            <img src={image} alt="image" className='absolute top-0 left-0 w-full h-full object-cover' />
-        </div>
-    )
+function Item({ title, price, image }) {
+  return (
+    <div className="w-full sm:w-[200px] h-[200px] overflow-hidden relative p-4 text-white embla__slide md:flex-2">
+      <div className="relative z-50">
+        <div>{title}</div>
+        <div>{price}</div>
+      </div>
+      <img
+        src={image}
+        alt="image"
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      />
+    </div>
+  );
 }
 
-export default Item
+export default Item;
