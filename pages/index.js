a739910853a11e2634f41f6d597668dcea2b919c
coronasco <daniel.zaharia.corona@gmail.com>
2021-08-15 16:03:49 +0200

    created deal of the day

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from "./../components/header";
 import Hero from "./../components/hero";
 import Categories from "../components/categories";
 import Featured from "../components/featured";
+import DealOfTheDay from "../components/dayDeal";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Hero />
         <Categories />
         <Featured />
+        <DealOfTheDay />
       </main>
     </div>
   );
