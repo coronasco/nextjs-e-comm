ec8425fd51f39fa0ea0cafbd93519cee7c9f4a7a
coronasco <daniel.zaharia.corona@gmail.com>
2021-08-15 11:11:57 +0200

    update design

--- components/item.js
+++ components/item.js
@@ -1,15 +1,15 @@
 function Item({ title, price, image }) {
   return (
-    <div className="w-full sm:w-[200px] h-[200px] overflow-hidden relative p-4 text-white embla__slide md:flex-2">
-      <div className="relative z-50">
-        <div>{title}</div>
-        <div>{price}</div>
-      </div>
+    <div className="w-full sm:w-[200px] p-4 text-white embla__slide md:flex-2">
       <img
         src={image}
         alt="image"
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="w-full h-full object-cover h-[300px]"
       />
+      <div className="relative z-50 text-gray-900 mt-5">
+        <div className="text-lg">{title}</div>
+        <div className="text-gray-700 font-bold text-sm">{price}</div>
+      </div>
     </div>
   );
 }
