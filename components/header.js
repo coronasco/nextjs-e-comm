5b868263e3e141221bf0fed5f2f935884dcd257e
coronasco <daniel.zaharia.corona@gmail.com>
2021-08-15 18:04:56 +0200

    some updates

--- components/header.js
+++ components/header.js
@@ -32,7 +32,7 @@ function Header() {
         </div>
       </div>
       <div className="px-4 md:px-0 container mx-auto">
-        <div className="grid grid-cols-3 flex items-center py-4">
+        <div className="flex justify-between items-center py-4">
           <div className="flex items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +50,7 @@ function Header() {
             </svg>
             <span className="font-bold ml-2">FlyShop</span>
           </div>
-          <div className="hidden md:flex gap-5 text-gray-600 justify-center">
+          <div className="hidden md:flex space-x-5 text-gray-600 justify-center">
             <Link href="/">
               <a>Home</a>
             </Link>
@@ -64,82 +64,84 @@ function Header() {
               <a>Contact</a>
             </Link>
           </div>
-          <div className="flex gap-3 text-gray-500 justify-center md:justify-end">
-            <div className="hover:text-gray-900 transition-all cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
+          <div className="flex items-center">
+            <div className="flex space-x-3 text-gray-500 justify-end">
+              <div className="hover:text-gray-900 transition-all cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </div>
+              <div className="hover:text-gray-900 transition-all cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  />
+                </svg>
+              </div>
+              <div className="hover:text-gray-900 transition-all cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </div>
             </div>
-            <div className="hover:text-gray-900 transition-all cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            <div className="flex justify-end ml-10 md:ml-0">
+              <div
+                className="h-[16px] w-5 flex flex-col justify-between md:hidden"
+                onClick={handleClick}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                />
-              </svg>
-            </div>
-            <div className="hover:text-gray-900 transition-all cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            </div>
-          </div>
-          <div className="flex justify-end">
-            <div
-              className="h-[16px] w-5 flex flex-col justify-between md:hidden"
-              onClick={handleClick}
-            >
-              <div
-                className={`${
-                  active
-                    ? "rotate-45 translate-y-[7px] duration-500 transition-all"
-                    : ""
-                } m-btn-line`}
-              ></div>
-              <div
-                className={`${
-                  active
-                    ? "transformt translate-x-[-10px] duration-500 opacity-0 transition-all"
-                    : ""
-                } m-btn-line`}
-              ></div>
-              <div
-                className={`${
-                  active
-                    ? "-rotate-45 translate-y-[-7px] duration-500 transition-all"
-                    : ""
-                } m-btn-line`}
-              ></div>
+                <div
+                  className={`${
+                    active
+                      ? "rotate-45 translate-y-[7px] duration-500 transition-all"
+                      : ""
+                  } m-btn-line`}
+                ></div>
+                <div
+                  className={`${
+                    active
+                      ? "transformt translate-x-[-10px] duration-500 opacity-0 transition-all"
+                      : ""
+                  } m-btn-line`}
+                ></div>
+                <div
+                  className={`${
+                    active
+                      ? "-rotate-45 translate-y-[-7px] duration-500 transition-all"
+                      : ""
+                  } m-btn-line`}
+                ></div>
+              </div>
             </div>
           </div>
         </div>
